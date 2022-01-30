04363f91fe72a78a4f49762b2321caf126028165
mucahitakbas <ma.mucahitakbas@gmail.com>
2022-01-30 15:56:04 +0300

    add antdesign

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useSelector } from "react-redux";
 import React from "react";
+import { Layout } from "antd";
 import Counter from "./components/Counter";
 import CounterAction from "./components/CounterAction";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Foter from "./components/Footer";
+import Head from "./components/Header";
 function App() {
+  const { Header, Footer, Content } = Layout;
   const main = useSelector((state) => state.main);
   return (
     <div className={main.dark ? "dark" : ""}>
-      <Header />
+      <Head />
       <Counter />
       <CounterAction />
-      <Footer />
+      <Foter />
     </div>
   );
 }
